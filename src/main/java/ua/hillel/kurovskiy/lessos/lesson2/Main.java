cc4802fcaaa4521aa7691d8570b1dc41ba6a8c34
Kurovskiy Vlad <kurovskiyvlad2005@gmail.com>
2024-02-13 20:30:25 +0200

    add all files

---
+++ src/main/java/ua/hillel/kurovskiy/lessos/lesson2/Main.java
@@ -0,0 +1,33 @@
+package ua.hillel.kurovskiy.lessos.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte width = 40;
+            char degree = '\u00B0';
+
+        byte width2 = 41;
+            char char1 = '\'';
+
+
+
+        double width3 = 20.57;
+            char char2 = '\u0022';
+
+        byte longitude = 74;
+
+        byte longitude2 = 02;
+
+        double longitude3 = 40.45;
+
+
+        System.out.print( "Система координат статуї свободи - " + width + degree + width2 + char1 + width3 + char2 + " N.L. " + "та " + longitude + degree + longitude2 + char1 + longitude3 + char2 + " W.L.");
+
+
+        //System.out.println(age2);
+
+
+
+    }
+
+}
---
+++ src/main/java/ua/hillel/kurovskiy/lessos/lesson3/Main.java
@@ -0,0 +1,7 @@
+package ua.hillel.kurovskiy.lessos.lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
