2feb8f45f4dc156a546661a2563c1cef6a4f2c47
Kurovskiy Vlad <kurovskiyvlad2005@gmail.com>
2024-02-27 22:42:53 +0200

    add all files

---
+++ src/main/java/ua/hillel/kurovskiy/lessons/lesson6/Main.java
@@ -0,0 +1,35 @@
+package ua.hillel.kurovskiy.lessons.lesson6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("                                    Гра - \"Вгадай число!\"");
+        System.out.println("Правила гри: \nКомп'ютер буде генерувати числа від 0 до 10, спробуйте за 5 спроб вгадати число!" );
+        System.out.println("Для початку натисніть \"Enter\"");
+
+        scanner.nextLine();
+
+        int Cargument = 0;
+        int Uargument = 0;
+
+        for (int i = 0; i < 5; i++) {
+            Cargument = (int) (Math.random() * 11);
+            System.out.println("Комп'ютер загадав число, введіть ваше значення нижче!" + Cargument);
+            Uargument = scanner.nextInt();
+            if (Uargument > 10){
+                System.out.println("Введіть значення у діапазоні від 1 до 10");
+                System.exit(0);
+            }
+            if (Uargument == Cargument){
+                System.out.println("Ви вгадали число! З перемогою!!!");
+                break;
+            }
+                System.out.println("Спробуйте ще раз)");
+                System.out.println();
+        }
+
+    }
+}
---
+++ src/main/java/ua/hillel/kurovskiy/lessons/lesson7/Main.java
@@ -0,0 +1,20 @@
+package ua.hillel.kurovskiy.lessons.lesson7;
+
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+//        int[] array = new int[10];
+////        array[0] = 4;
+////
+////        System.out.println(Arrays.toString(array));
+//        for (int i = 0; i < array.length; i++) {
+//            array[i] = (int) (Math.random() * 11);
+//            System.out.print(array[i] + ", ");
+//
+//        }
+
+
+    }
+}
